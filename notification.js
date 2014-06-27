7b2443efadb4a543dfa1497433bda1233de40d61
skore13 <skorepa.jakub@gmail.com>
2014-06-27 13:04:19 +0200

    Update notification.js

--- notification.js
+++ notification.js
@@ -1,111 +1,27 @@
 // LIBRARY
-/** @license
- * WebNotification.js <https://github.com/auchenberg/WebNotification.js/>
- * Released under the MIT license
- * Author: Kenneth Auchenberg
- * Version: 0.1.0
- */
- /* global define */
-
-
-;(function(window) {
-
-  'use strict';
-
-  var NativeNotification = window.Notification,
-      PrefixedNotification = window.webkitNotifications;
-
-  var utils = {
-    isFunction: function(obj) {
-      return typeof obj === 'function';
-    }
-  };
-
-  function WebNotification(title, options) {
-    return new NativeNotification(title, options);
-  }
-
-  WebNotification.getPermission = function() {
-
-    if (NativeNotification) {
-
-      // Official W3C/WHATWG Web Notifications API
-      if (NativeNotification.permission) {
-        return NativeNotification.permission;
-      }
-
-      // Older WebKit API
-      if (utils.isFunction(NativeNotification.permissionLevel)) {
-        return NativeNotification.permissionLevel();
-      }
-    }
-
-    // Oldprefixed WebKit API
-    if (PrefixedNotification && utils.isFunction(PrefixedNotification.checkPermission)) {
-      switch (PrefixedNotification.checkPermission()) {
-        case 0:
-          return 'granted';
-        case 1:
-          return 'default';
-        case 2:
-          return 'denied';
-      }
-    }
-
-    return 'default';
-  };
-
-  WebNotification.requestPermission = function(callback) {
-    var context = this;
-
-    function _onPermissionRequested() {
-      context.permission = context.getPermission();
-
-      if (callback) {
-        callback.call(context);
-      }
-    }
-
-    // Native first, then prefxied
-    if (NativeNotification && utils.isFunction(NativeNotification.requestPermission)) {
-      NativeNotification.requestPermission(_onPermissionRequested);
-    } else if (PrefixedNotification && utils.isFunction(PrefixedNotification.requestPermission)) {
-      PrefixedNotification.requestPermission(_onPermissionRequested);
-    } else {
-      throw 'Could not call requestPermission';
-    }
-
-  };
-
-  WebNotification.permission = WebNotification.getPermission();
-
-  if (typeof define === 'function' && define.amd) {
-    define(function() {
-      if('Notification' in window) {
-        window.Notification = WebNotification;
-        return WebNotification;
-      }
-    });
-  } else {
-    if('Notification' in window) {
-      window.Notification = WebNotification;
-    }
-  }
-
-})(this);
+(function(d, script) {
+    script = d.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.onload = function(){
+        // when clicked on news ask for permissions
+        document.getElementById("commentsText").addEventListener("click", function() {
+            window.Notification.requestPermission(function() {
+                alert('Permissions state: ' + window.Notification.permission);
+            });
+        });
+        
+        // edit goldenCookie.spawn function so it shows notification
+        var cached_function = Game.goldenCookie.spawn;
+        Game.goldenCookie.spawn=function() {
+            cached_function.apply(this);
+            new window.Notification("Golden cookie spawned");
+        }
+    };
+    script.src = 'https://rawgit.com/auchenberg/WebNotification.js/master/WebNotification.js';
+    d.getElementsByTagName('head')[0].appendChild(script);
+}(document));
 
 // MY CODE
 
-// when clicked on news ask for permissions
-document.getElementById("commentsText").addEventListener("click", function() {
-  window.Notification.requestPermission(function() {
-    alert('Permissions state: ' + window.Notification.permission);
-  });
-});
 
-// edit goldenCookie.spawn function so it shows notification
-var cached_function = Game.goldenCookie.spawn;
-Game.goldenCookie.spawn=function() {
-  cached_function.apply(this);
-  new window.Notification("Golden cookie spawned");
-}
